0869b9cf94d5e0861d724c3ac9d2e4c01d0cbe39
Dowee Maynard <Dowee24@gmail.com>
2021-11-23 07:47:01 -0500

    FolderStructure for sound Effects

---
+++ SpaceInvaders/Model/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media;
+
+namespace SpaceInvaders.Model
+{
+    public class SoundManager
+    {
+        public void Soundtest()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"c:\Windows\Media\chimes.wav");
+
+            WindowsMediaPlayer player = new WindowsMediaPlayer();
+            System.Media.SoundPlayer player = new SoundPlayer();
+            simpleSound.Play();
+
+        }
+    }
+}
